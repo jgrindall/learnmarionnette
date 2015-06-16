a7efb327890dad9fbf617250a259b745e73378c8
John <john.grindall@gmail.com>
2015-06-16 19:11:22 +0100

    playing with commands and requires

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,6 @@ app.vent.on("search:term", function(searchTerm){
     Backbone.history.navigate("search/" + searchTerm);
 });
 
-app.reqres.setHandler("test", function(data){
-    return data.num  * 7;
-});
-
 app.search = function(term){
     app.vent.trigger("search:term", term);
 };
@@ -18,5 +14,26 @@ app.on("start", function(){
     Backbone.history.start();
 });
 
+var globalChannel = Backbone.Wreqr.radio.channel('global');
 
+var userChannel = Backbone.Wreqr.radio.channel('user');
+
+userChannel.commands.setHandler("commandname", function(data){
+    console.log("user command");
+});
+
+userChannel.reqres.setHandler("requestname", function(data){
+    console.log("user requestname");
+});
+
+globalChannel.vent.on('trigger1', function(){
+    console.log("global channel trigger1");
+});
+
+globalChannel.commands.execute('commandname' );
+globalChannel.reqres.request('requestname' );
+globalChannel.vent.trigger('trigger1');
+userChannel.commands.execute('commandname');
+userChannel.reqres.request('requestname');
+userChannel.vent.trigger('trigger2');
 
